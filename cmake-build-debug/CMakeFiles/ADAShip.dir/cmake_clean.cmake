94785792b716f3256d43c313d9a73ed29774e68a
Borislav D <daniel.dochev@ada.ac.uk>
2021-03-04 13:33:35 +0200

    Running on Clion now

---
+++ cmake-build-debug/CMakeFiles/ADAShip.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "ADAShip"
+  "ADAShip.pdb"
+  "CMakeFiles/ADAShip.dir/src/Ship.cpp.o"
+  "CMakeFiles/ADAShip.dir/src/configurationReader.cpp.o"
+  "CMakeFiles/ADAShip.dir/src/gameController.cpp.o"
+  "CMakeFiles/ADAShip.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ADAShip.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
